4f8be71b5d54a7d8da6804d9fe26dde39b3bd788
CarbonMan <CarbonMan@users.noreply.github.com>
2021-12-04 16:23:35 +1100

    Update CentaurInternalPOD-0.1.js

--- scripts/CentaurInternalPOD/CentaurInternalPOD-0.1.js
+++ scripts/CentaurInternalPOD/CentaurInternalPOD-0.1.js
@@ -1,1 +1,13 @@
 console.log("Centaur internal POD loaded");
+function InternalPOD(){
+  if (typeof configuration != "undefined"){
+    // In the Config.html page
+    this.setup = new InternalPOD.prototype.Setup();
+  }
+  configuration.plugins['Centaur_I_POD'] = this;
+}
+
+InternalPOD.prototype.Setup = function(){
+};
+
+var internalPOD = new InternalPOD();
