03ced7d94835fa886d3128f9c1516f3e2a42f0be
CarbonMan <CarbonMan@users.noreply.github.com>
2022-03-07 11:07:51 +1100

    sha256

--- scripts/emailPOD/emailTransfers.js
+++ scripts/emailPOD/emailTransfers.js
@@ -81,6 +81,9 @@ class EmailTransfers {
                 }
             }
         });
+        // sha256 to stop tampering
+        let hash = sha256(body);
+        body += '\n\nCHECK: ' + hash;
         cordova.plugins.email.open({
             to: this.emailAddress,
             subject: `Job # ${request.reference}`,
@@ -155,3 +158,19 @@ class EmailTransfers {
     }
 
 }
+
+async function sha256(message) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder('utf-8').encode(message);
+  
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+  
+    // convert bytes to hex string
+    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+    console.log(hashHex);
+    return hashHex;
+  }
