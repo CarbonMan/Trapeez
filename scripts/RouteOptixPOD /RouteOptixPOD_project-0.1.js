327508846cdb8c8b73a2eb6eef42a2c622c1a3ff
CarbonMan <CarbonMan@users.noreply.github.com>
2025-07-08 20:49:19 +1000

    Create RouteOptixPOD_project-0.1.js

---
+++ scripts/RouteOptixPOD /RouteOptixPOD_project-0.1.js
@@ -0,0 +1,49 @@
+function RouteOptixProject(){
+  document.addEventListener('LOAD_PLUGIN_ACTIONS', function (ev) {
+    // Attach a POD action onto the actions combobox in the configuration page
+    // ev.detail.actions.push({
+    //   name: "POD",
+    //   value: "DROP"
+    // });
+  });
+}
+
+/**
+* Config is used only in the configuration page
+* for editing settings.
+*/
+RouteOptix.prototype.Config = function(){
+  /**
+  * Create the interface in the app for modifying 
+  * the Centaur POD options
+  */
+  this.options = function($div, instance){
+    let settings = `<div class="field">
+        <label>Username</label>
+        <input type="text" class="instanceForm" id="routeOptixUsername" placeholder="user name" value="${instance.routeOptixUsername || ''}">
+      </div><div class="field">
+        <label>Password</label>
+        <input type="text" class="instanceForm" id="routeOptixPassword" placeholder="password" value="${instance.routeOptixPassword || ''}">
+      </div><div class="field">
+        <label>Server host</label>
+        <input type="text" class="instanceForm" id="routeOptixHost" placeholder="host" value="${instance.routeOptixHost || ''}">
+      </div>`;
+    $div.html(settings);
+  };
+
+  /**
+  * Called by the app when the user clicks save
+  */
+  this.saveOptions = function(instance){
+      instance.routeOptixHost = $("#routeOptixHost").val();
+      instance.routeOptixUsername = $("#routeOptixUsername").val();
+      instance.routeOptixPassword = $("#routeOptixPassword").val();
+  };
+  
+};
+
+
+document.addEventListener('ROUTE_OPTIX_READY', function (e) {
+  console.log('Centaur project added');
+  e.detail.project = new RouteOptixProject();
+}, false);
