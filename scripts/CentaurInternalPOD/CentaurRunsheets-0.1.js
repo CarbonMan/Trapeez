4345ff8e16b81cddefa99b0bde92ea7fc35a9edf
CarbonMan <CarbonMan@users.noreply.github.com>
2025-08-18 15:52:30 +1000

    Update CentaurRunsheets-0.1.js

--- scripts/CentaurInternalPOD/CentaurRunsheets-0.1.js
+++ scripts/CentaurInternalPOD/CentaurRunsheets-0.1.js
@@ -1,10 +1,24 @@
-InternalPOD.prototype.Runsheets = function(opts){
-  const action = sessionStorage.getItem('action');
-  scanner.on('scan', (ev)=>{
-      console.log('InternalPOD - Scan event trapped');
-      if (action && action != 'CENTAUR_RUNSHEETS'){
-        return;
-      }
-    ev.data.target = 'frame.html';
-  });
-}
+InternalPOD.prototype.Runsheets = class{
+  constructor(){
+    const action = sessionStorage.getItem('action');
+    if (location.href == 'index.html'){
+      scanner.on('scan', (ev)=>{
+          console.log('InternalPOD - Scan event trapped');
+          if (action && action != 'CENTAUR_RUNSHEETS'){
+            return;
+          }
+          // Change the destination to frame.html
+          ev.data.target = 'frame.html';
+      });
+    }
+  }
+};
+let runsheets = null;
+document.addEventListener('PLUGIN_LOADED', function (ev) {
+  // From src\www\js\scriptManagement.js
+  if (ev.detail.name == 'InternalPOD'){
+    //const internalPOD = $T.getInstanceByName({name: 'InternalPOD'});
+    runsheets = ev.detail.instance.Runsheets();
+    console.log('internalPOD.Runsheets plugin instantiated');
+  }
+});
