4ccd9cc27ef0ae5785c638dd0be2bfa9763363b5
CarbonMan <CarbonMan@users.noreply.github.com>
2025-10-26 21:37:41 +1100

    Change jobId to rnHeader_mb in table row click

--- scripts/CentaurInternalPOD/CentaurRunsheets-0.1.js
+++ scripts/CentaurInternalPOD/CentaurRunsheets-0.1.js
@@ -156,9 +156,9 @@ InternalPOD_Runsheets = class {
 
         for (let tuple of tuples) {
             const refno = tuple.getElementsByTagName("REFNO")[0]?.textContent || '';
-            const jobId = tuple.getElementsByTagName("CONNOTE")[0]?.textContent || '';
+            const rnHeader_mb = tuple.getElementsByTagName("AGGREGATE_ID")[0]?.textContent || '';
             tableHtml += `
-	        <tr onclick="runsheets.processItem('${jobId}')" style="cursor: pointer;">
+	        <tr onclick="runsheets.processItem('${rnHeader_mb}')" style="cursor: pointer;">
 	            <td>${tuple.getElementsByTagName("MAIL_BOX_CODE")[0]?.textContent || 'UNKNOWN'}</td>
 	            <td>${tuple.getElementsByTagName("ITEMS")[0]?.textContent || ''}</td>
 	        </tr>`;
